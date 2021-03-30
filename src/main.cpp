739ad96fce328294376d04a71d1b726409928a57
Dominik Horn <dominik.horn@tum.de>
2021-03-30 16:53:44 +0200

    test xxh3 integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 
-#include "hashing.hpp"
 #include "xxHash.hpp"
 
 int main() {
     // TODO: temporary test
     for (unsigned long long i = 0; i < 32; i++) {
-        std::cout << i << " |--xxHash--> " << XXH64_hash(i) << std::endl;
+        std::cout << i << " |--XXH64--> " << XXH64_hash(i) << std::endl;
+        std::cout << i << " |--XXH3---> " << XXH3_hash(i) << std::endl;
     }
 
     return 0;
