523f0e27e0484de06a2b63bf26d7c4811a19d72e
Dominik Horn <dominik.horn@tum.de>
2021-03-31 18:52:50 +0200

    reformat hashing.hpp

--- src/include/hashing.hpp
+++ src/include/hashing.hpp
@@ -11,8 +11,8 @@
 typedef unsigned long HASH_32;
 typedef unsigned long long HASH_64;
 typedef struct packed {
-    HASH_64 low64;
-    HASH_64 high64;
+   HASH_64 low64;
+   HASH_64 high64;
 } HASH_128;
 
 /**
@@ -22,34 +22,34 @@ typedef struct packed {
  */
 #include "../../hash_functions/xxHash/xxhash.h"
 
-template <typename T>
+template<typename T>
 HASH_32 forceinline XXH32_hash(T value, XXH32_hash_t seed = 0) {
-    return XXH32(&value, sizeof(value), seed);
+   return XXH32(&value, sizeof(value), seed);
 }
 
-template <typename T>
+template<typename T>
 HASH_64 forceinline XXH64_hash(T value, XXH64_hash_t seed = 0) {
-    return XXH64(&value, sizeof(T), seed);
+   return XXH64(&value, sizeof(T), seed);
 }
 
-template <typename T>
+template<typename T>
 HASH_64 forceinline XXH3_hash(T value) {
-    return XXH3_64bits(&value, sizeof(T));
+   return XXH3_64bits(&value, sizeof(T));
 }
 
-template <typename T>
+template<typename T>
 HASH_64 forceinline XXH3_hash_withSeed(T value, XXH64_hash_t seed = 0) {
-    return XXH3_64bits_withSeed(&value, sizeof(T), seed);
+   return XXH3_64bits_withSeed(&value, sizeof(T), seed);
 }
 
-template <typename T>
+template<typename T>
 HASH_128 forceinline XXH3_128_hash(T value) {
-    return XXH3_128bits(&value, sizeof(T));
+   return XXH3_128bits(&value, sizeof(T));
 }
 
-template <typename T>
+template<typename T>
 HASH_128 forceinline XXH3_128_hash_withSeed(T value, XXH64_hash_t seed = 0) {
-    return XXH3_128bits_withSeed(&value, sizeof(T), seed);
+   return XXH3_128bits_withSeed(&value, sizeof(T), seed);
 }
 
 // TODO: expose other xxhash functionality, especially batching/streaming
