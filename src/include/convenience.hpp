885be8d9478fd003580ee943c6dd20cc6d02504b
Dominik Horn <dominik.horn@tum.de>
2021-04-01 17:35:50 +0200

    add more useful compiler hints

--- src/include/convenience.hpp
+++ src/include/convenience.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
 #ifdef __GNUC__
-   #define forceinline __attribute__((always_inline))
    #define packed __attribute__((packed))
+
+   /**
+    * The following compiler hints were taken from
+    * https://github.com/toschmidt/cpp-utility/blob/master/src/cpp-utility/compiler/CompilerHints.hpp#L8
+    * (commit: 46a5c6b)
+    */
+   #define forceinline inline __attribute__((always_inline))
+   #define unroll_loops __attribute__((optimize("unroll-loops")))
+   #define likely(expr) __builtin_expect((expr), 1)
+   #define unlikely(expr) __builtin_expect((expr), 0)
 #endif
