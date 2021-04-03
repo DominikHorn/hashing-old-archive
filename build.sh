ced1235da91e45870a1c20abb7bc695f8ea263a5
Dominik Horn <dominik.horn@tum.de>
2021-04-03 16:23:27 +0200

    upgrade build script

--- build.sh
+++ build.sh
@@ -6,16 +6,26 @@ CLANGPP_BIN=clang++
 GCC_BIN=gcc-10
 GCCPP_BIN=g++-10
 
+# Stop on error & cd to script directory
+set -e
+cd "$(dirname "$0")"
+
 # build with clang
-cmake . \
- -D CMAKE_C_COMPILER=${CLANG_BIN} \
- -D CMAKE_CXX_COMPILER=${CLANGPP_BIN}
+cmake \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_C_COMPILER=${CLANG_BIN} \
+  -D CMAKE_CXX_COMPILER=${CLANGPP_BIN} \
+  -G "CodeBlocks - Unix Makefiles" \
+  .
 cmake --build . --clean-first -DCMAKE_BUILD_TYPE=Release
 mv src/main benchmark-clang
 
 # build with gcc
-cmake . \
+cmake \
+  -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_C_COMPILER=${GCC_BIN} \
-  -D CMAKE_CXX_COMPILER=${GCCPP_BIN}
+  -D CMAKE_CXX_COMPILER=${GCCPP_BIN} \
+  -G "CodeBlocks - Unix Makefiles" \
+  .
 cmake --build . --clean-first -DCMAKE_BUILD_TYPE=Release
 mv src/main benchmark-gcc
