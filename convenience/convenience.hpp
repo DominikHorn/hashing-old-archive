8030dfe2f5389512fd239c9e5d72133b0461869a
Dominik Horn <dominik.horn@tum.de>
2021-04-03 12:31:14 +0200

    disable optimizations like forceinline for debug builds
    this makes reading the disassembly easier. Flip side of the coin: manual
    disassembly is necessary to understand if we're actually benchmarking
    correctly inlined functions.

--- convenience/convenience.hpp
+++ convenience/convenience.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
-#ifdef __GNUC__
-   #define packed __attribute__((packed))
+// Only perform these optimizations for release builds
+#ifdef NDEBUG
+   #ifdef __GNUC__
+      #define packed __attribute__((packed))
 
-   /**
+      /**
     * The following compiler hints were taken from
     * https://github.com/toschmidt/cpp-utility/blob/master/src/cpp-utility/compiler/CompilerHints.hpp#L8
     * (commit: 46a5c6b)
     */
-   #define forceinline inline __attribute__((always_inline))
-   #define unroll_loops __attribute__((optimize("unroll-loops")))
-   #define likely(expr) __builtin_expect((expr), 1)
-   #define unlikely(expr) __builtin_expect((expr), 0)
+      #define forceinline inline __attribute__((always_inline))
+      #define unroll_loops __attribute__((optimize("unroll-loops")))
+      #define likely(expr) __builtin_expect((expr), 1)
+      #define unlikely(expr) __builtin_expect((expr), 0)
+   #endif
+#else
+   #define forceinline
+   #define unroll_loops
+   #define likely(expr)
+   #define unlikely(expr)
 #endif
