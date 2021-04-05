0a8bac3964626ffc9c0a5a06d93ff97d82ff4ed2
Dominik Horn <dominik.horn@tum.de>
2021-04-05 14:25:01 +0200

    match main.cpp output arrow lengths

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,20 @@ int main(const int argc, const char* argv[]) {
       for (uint64_t i = 0; i < 32; i++) {
          const auto val = args.dataset[i];
          std::cout << std::endl;
-         std::cout << val << " |--XXH64--> " << XXHash::XXH64_hash(val) << std::endl;
-         std::cout << val << " |--XXH3---> " << XXHash::XXH3_hash(val) << std::endl;
+         std::cout << val << " |--XXH64--------> " << XXHash::XXH64_hash(val) << std::endl;
+         std::cout << val << " |--XXH3---------> " << XXHash::XXH3_hash(val) << std::endl;
 
          std::cout << std::endl;
-         std::cout << val << " |--mult32---> " << MultHash::mult32_hash(val) << std::endl;
-         std::cout << val << " |--mult64--> " << MultHash::mult64_hash(val) << std::endl;
-         std::cout << val << " |--fibo32---> " << MultHash::fibonacci32_hash(val) << std::endl;
-         std::cout << val << " |--fibo64---> " << MultHash::fibonacci64_hash(val) << std::endl;
+         std::cout << val << " |--mult32-------> " << MultHash::mult32_hash(val) << std::endl;
+         std::cout << val << " |--mult64-------> " << MultHash::mult64_hash(val) << std::endl;
+         std::cout << val << " |--fibo32-------> " << MultHash::fibonacci32_hash(val) << std::endl;
+         std::cout << val << " |--fibo64-------> " << MultHash::fibonacci64_hash(val) << std::endl;
          std::cout << val << " |--fiboPrime32--> " << MultHash::fibonacci_prime32_hash(val) << std::endl;
          std::cout << val << " |--fiboPrime64--> " << MultHash::fibonacci_prime64_hash(val) << std::endl;
 
          std::cout << std::endl;
-         std::cout << val << " |--multadd32--> " << MultAddHash::multadd32_hash(val) << std::endl;
-         std::cout << val << " |--multadd64--> " << MultAddHash::multadd64_hash(val) << std::endl;
+         std::cout << val << " |--multadd32----> " << MultAddHash::multadd32_hash(val) << std::endl;
+         std::cout << val << " |--multadd64----> " << MultAddHash::multadd64_hash(val) << std::endl;
 
          std::cout << std::endl;
          std::cout << val << " |--murmur32_fin--> " << MurmurHash3::finalize_32(val) << std::endl;
