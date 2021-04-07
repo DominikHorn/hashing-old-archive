e9cc938cd473635315cca1673fe1be95ccc1cf81
Dominik Horn <dominik.horn@tum.de>
2021-04-07 11:55:51 +0200

    reorganize convenience defs & implement prefetching

--- convenience/convenience.hpp
+++ convenience/convenience.hpp
@@ -1,23 +1,65 @@
 #pragma once
 
-// Only perform these optimizations for release builds
-#ifdef NDEBUG
-   #ifdef __GNUC__
-      #define packed __attribute__((packed))
-
-      /**
-    * The following compiler hints were taken from
+#ifdef __GNUC__
+   /**
+    * These optimizing compiler hints were taken from
     * https://github.com/toschmidt/cpp-utility/blob/master/src/cpp-utility/compiler/CompilerHints.hpp#L8
     * (commit: 46a5c6b)
+    *
+    * We only want to perform them in release builds
     */
+   #ifdef NDEBUG
       #define forceinline inline __attribute__((always_inline))
       #define unroll_loops __attribute__((optimize("unroll-loops")))
       #define likely(expr) __builtin_expect((expr), 1)
       #define unlikely(expr) __builtin_expect((expr), 0)
+   #else
+      #define forceinline
+      #define unroll_loops
+      #define likely(expr)
+      #define unlikely(expr)
    #endif
+
+   #define packed __attribute__((packed))
+   #define prefetch(address, mode, locality) __builtin_prefetch(address, mode, locality)
 #else
-   #define forceinline
-   #define unroll_loops
-   #define likely(expr)
-   #define unlikely(expr)
+   #error GNUC is the only supported compiler atm
 #endif
+
+struct Prefetcher {
+   enum Locality
+   {
+      NONE = 0,
+      LOW = 1,
+      MEDIUM = 2,
+      HIGH = 3
+   };
+   enum Mode
+   {
+      READ = 0,
+      WRITE = 1
+   };
+
+   /**
+    * Prefetches a cache line starting at address, if possible on CPU
+    */
+   static void forceinline prefetch_address(const void* address, Mode mode = Mode::WRITE,
+                                            Locality locality = Locality::NONE) {
+      prefetch(address, mode, locality);
+   }
+
+   /**
+    * Prefetches a block of data starting at address with given size.
+    * NOTE: does not guarantee that the entire block actually is in cache afterwards
+    * (missing prefetch instruction, evictions we don't control etc)
+    *
+    * @tparam CacheLineSize cache line size in bytes. Defaults to 64
+    */
+   template<size_t CacheLineSize = 64>
+   static void forceinline prefetch_block(const void* address, const size_t size, Mode mode = Mode::WRITE,
+                                          Locality locality = Locality::NONE) {
+      unroll_loops for (auto ptr = (uint8_t*) address; ptr < (uint8_t*) address + size; ptr += CacheLineSize) {
+         prefetch(ptr, mode, locality);
+      }
+   }
+};
