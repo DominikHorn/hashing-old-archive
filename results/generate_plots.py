58cd22550a9409015e7a145c8239b04ff9bb16a5
Dominik Horn <dominik.horn@tum.de>
2021-04-13 21:15:29 +0200

    ensure consistent color schema across charts

--- results/generate_plots.py
+++ results/generate_plots.py
@@ -1,14 +1,19 @@
 from collections import OrderedDict
 
-import matplotlib as mpl
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pandas
 
-colors = list(mpl.colors.TABLEAU_COLORS)
-
 csv = pandas.read_csv('throughput.csv')
 dataset_names = sorted(set(csv['dataset']))
+
+# ensure colors are at least consistent across plots
+tableau_colors = list(matplotlib.colors.TABLEAU_COLORS)
+hash_methods = list(
+    OrderedDict.fromkeys(list(
+        csv[csv['load_factor'] == 1.0][csv['reducer'] == 'do_nothing'].sort_values('nanoseconds_per_key')['hash'])))
+colors = {method: tableau_colors[i % len(tableau_colors)] for i, method in enumerate(hash_methods)}
 
 for fig, dataset_name in enumerate(dataset_names):
     dataset = csv[csv['dataset'] == dataset_name]
@@ -25,17 +30,14 @@ for fig, dataset_name in enumerate(dataset_names):
     plt.xlabel('hash reduction method')
 
     # order preserving deduplication
-    hash_methods = list(OrderedDict.fromkeys(list(dataset.sort_values('nanoseconds_per_key')['hash'])))
-
-    for j, hash_name in enumerate(hash_methods):
+    for j, hash_name in enumerate([m for m in hash_methods if m in set(dataset['hash'])]):
         series = list(dataset[dataset['hash'] == hash_name].sort_values('reducer')['nanoseconds_per_key'])
         width = 0.95 / len(hash_methods)
 
         for i, reducer in enumerate(reducers):
-            if i == 0:
-                plt.bar(i + j * width, series[i], width, label=hash_name, color=colors[j % len(colors)])
-            else:
-                plt.bar(i + j * width, series[i], width, color=colors[j % len(colors)])
+            # We only want to set label once as otherwise legend will contain duplicates
+            plt.bar(i + j * width, series[i], width, label=hash_name if i == 0 else None,
+                    color=colors.get(hash_name) or "red")
 
     plt.legend(bbox_to_anchor=(0.5, -0.1), ncol=7)
     plt.savefig(f"throughput_{dataset_name}.pdf", bbox_inches='tight', pad_inches=0.5)
