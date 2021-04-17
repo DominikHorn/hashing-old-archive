d05d704a7b5d493338057667fbcfe4787e94f620
Dominik Horn <dominik.horn@tum.de>
2021-04-17 15:22:46 +0200

    don't throughput benchmark different load factors

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,6 @@ cmake-build-release/src/collisions \
   -outfile=results/collision.csv
 
 cmake-build-release/src/throughput \
-  -loadfactors=1.0,0.75,0.5,0.25 \
   -datasets=books_200M_uint32.ds:4,books_200M_uint64.ds:8,fb_200M_uint64.ds:8,osm_cellids_200M_uint64.ds:8,wiki_ts_200M_uint64.ds:8 \
   -datapath=data/ \
   -outfile=results/throughput.csv
