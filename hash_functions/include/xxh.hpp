85dfa1bff184b93b04381ad61ca200dbabf3e0d6
Dominik Horn <dominik.horn@tum.de>
2021-04-17 17:20:08 +0200

    ensure xxh3 is actually inlined
    sadly this necessitated copying xxhash code since it improperly
    defined inlining, which resulted in >= 1 callq in the tight throughput
    loop.

--- hash_functions/include/xxh.hpp
+++ hash_functions/include/xxh.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <convenience.hpp>
-
-// enable full inlining for xxhash code
-#define XXH_INLINE_ALL
-#include "external/xxHash/xxhash.h"
+#include <thirdparty/xxhash.h>
 
 #include "types.hpp"
 
@@ -14,50 +11,49 @@
  *           xxHash
  * ----------------------------
  */
-
 // TODO: benchmark with clang & gcc builds as clang supposedly improves xxHash performance
 struct XXHash {
-   template<typename T>
-   static constexpr forceinline HASH_32 XXH32_hash(const T& value) {
-      return XXH32(&value, sizeof(value), 0);
-   }
+    template<typename T>
+    static constexpr forceinline HASH_32 XXH32_hash(const T& value) {
+        return _XXHash::XXH32(&value, sizeof(value), 0);
+    }
 
-   template<typename T>
-   static constexpr forceinline HASH_32 XXH32_hash_withSeed(const T& value, HASH_32& seed) {
-      return XXH32(&value, sizeof(value), seed);
-   }
+    template<typename T>
+    static constexpr forceinline HASH_32 XXH32_hash_withSeed(const T& value, HASH_32& seed) {
+        return _XXHash::XXH32(&value, sizeof(value), seed);
+    }
 
-   template<typename T>
-   static constexpr forceinline HASH_64 XXH64_hash(const T& value) {
-      return XXH64(&value, sizeof(value), 0);
-   }
+    template<typename T>
+    static constexpr forceinline HASH_64 XXH64_hash(const T& value) {
+        return _XXHash::XXH64(&value, sizeof(value), 0);
+    }
 
-   template<typename T>
-   static constexpr forceinline HASH_64 XXH64_hash_withSeed(const T& value, HASH_64& seed) {
-      return XXH64(&value, sizeof(value), seed);
-   }
+    template<typename T>
+    static constexpr forceinline HASH_64 XXH64_hash_withSeed(const T& value, HASH_64& seed) {
+        return _XXHash::XXH64(&value, sizeof(value), seed);
+    }
 
-   template<typename T>
-   static constexpr forceinline HASH_64 XXH3_hash(const T& value) {
-      return XXH3_64bits(&value, sizeof(T));
-   }
+    template<typename T>
+    static constexpr forceinline HASH_64 XXH3_hash(const T& value) {
+        return _XXHash::XXH3_64bits(&value, sizeof(T));
+    }
 
-   template<typename T>
-   static constexpr forceinline HASH_64 XXH3_hash_withSeed(const T& value, HASH_64& seed) {
-      return XXH3_64bits_withSeed(&value, sizeof(T), seed);
-   }
+    template<typename T>
+    static constexpr forceinline HASH_64 XXH3_hash_withSeed(const T& value, HASH_64& seed) {
+        return _XXHash::XXH3_64bits_withSeed(&value, sizeof(T), seed);
+    }
 
-   template<typename T>
-   static forceinline HASH_128 XXH3_128_hash(const T& value) {
-      // TODO: is this the proper 128 bit XXH variant?
-      const auto val = XXH3_128bits(&value, sizeof(T));
-      return {val.low64, val.high64};
-   }
+    template<typename T>
+    static forceinline HASH_128 XXH3_128_hash(const T& value) {
+        // TODO: is this the proper 128 bit XXH variant?
+        const auto val = _XXHash::XXH3_128bits(&value, sizeof(T));
+        return {val.low64, val.high64};
+    }
 
-   template<typename T>
-   static forceinline HASH_128 XXH3_128_hash_withSeed(const T& value, HASH_64& seed) {
-      // TODO: is this the proper 128 bit XXH variant?
-      const auto val = XXH3_128bits_withSeed(&value, sizeof(T), seed);
-      return {val.low64, val.high64};
-   }
+    template<typename T>
+    static forceinline HASH_128 XXH3_128_hash_withSeed(const T& value, HASH_64& seed) {
+        // TODO: is this the proper 128 bit XXH variant?
+        const auto val = _XXHash::XXH3_128bits_withSeed(&value, sizeof(T), seed);
+        return {val.low64, val.high64};
+    }
 };
