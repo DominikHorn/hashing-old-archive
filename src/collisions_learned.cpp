ccd464c0e3fc86003244ea502d13f3503c1cfa7c
Dominik Horn <dominik.horn@tum.de>
2021-04-19 11:44:15 +0200

    fix pgm EXC_BAD_ACCESS due to sample not containing min/max element
    e.g., pgm::search assumes that more segments always exist if the queried key
    is larger than the current segment's key. This is wrong however if we
    build pgm on a sample and that sample happens to not contain the
    largest/smallest key.

--- src/collisions_learned.cpp
+++ src/collisions_learned.cpp
@@ -52,88 +52,73 @@ static void measure(const std::string& dataset_name, const std::vector<uint64_t>
 
    // lambda to measure a given hash function with a given reducer. Will be entirely inlined by default
    std::vector<uint32_t> collision_counter(hashtable_size);
-   const auto measure_model = [&](const std::string& model_name, const auto& preparefn, const auto& buildfn,
-                                  const auto& modelfn, const std::string& reducer_name, const auto& reducerfn) {
-      for (const auto sample_size : args.sample_sizes) {
-         // Take a random sample
-         auto start_time = std::chrono::steady_clock::now();
-         const auto sample_n = static_cast<size_t>(sample_size * static_cast<long double>(dataset.size()));
-         if (sample_n == 0)
-            continue;
-         std::vector<uint64_t> sample(sample_n, 0);
+   const auto measure_model = [&](const std::string& model_name, const auto& samplefn, const auto& preparefn,
+                                  const auto& buildfn, const auto& modelfn, const std::string& reducer_name,
+                                  const auto& reducerfn) {
+      // Take a random sample
+      auto start_time = std::chrono::steady_clock::now();
+      auto sample = samplefn();
+      uint64_t sample_ns = static_cast<uint64_t>(
+         std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time).count());
 
-         const uint64_t seed = 0x9E3779B9LU; // Random constant to ensure reproducibility
-         std::default_random_engine gen(seed);
-         std::uniform_int_distribution<uint64_t> dist(0, dataset.size() - 1);
-         for (size_t i = 0; i < sample_n; i++) {
-            const auto random_index = dist(gen);
-            sample[i] = dataset[random_index];
-         }
-         uint64_t sample_ns = static_cast<uint64_t>(
-            std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time)
-               .count());
+      // Prepare the sample
+      start_time = std::chrono::steady_clock::now();
+      preparefn(sample);
+      uint64_t prepare_ns = static_cast<uint64_t>(
+         std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time).count());
 
-         // Prepare the sample
-         start_time = std::chrono::steady_clock::now();
-         preparefn(sample);
-         uint64_t prepare_ns = static_cast<uint64_t>(
-            std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time)
-               .count());
+      // Build the model
+      start_time = std::chrono::steady_clock::now();
+      const auto model = buildfn(sample);
+      uint64_t build_ns = static_cast<uint64_t>(
+         std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time).count());
 
-         // Build the model
-         start_time = std::chrono::steady_clock::now();
-         const auto model = buildfn(sample);
-         uint64_t build_ns = static_cast<uint64_t>(
-            std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_time)
-               .count());
+      // Measure
+      const auto stats = Benchmark::measure_collisions(
+         dataset, collision_counter, [&](const HASH_64& key) { return modelfn(model, hashtable_size, key); },
+         reducerfn);
 
-         // Measure
-         const auto stats = Benchmark::measure_collisions(
-            dataset, collision_counter,
-            [&](const HASH_64& key) { return modelfn(model, sample_n, hashtable_size, key); }, reducerfn);
-
-         // Sum up for easier access
-         const auto total_ns = sample_ns + prepare_ns + build_ns + stats.inference_reduction_memaccess_total_ns;
+      // Sum up for easier access
+      const auto total_ns = sample_ns + prepare_ns + build_ns + stats.inference_reduction_memaccess_total_ns;
 
 #ifdef VERBOSE
-         {
-            std::unique_lock<std::mutex> lock(iomutex);
-            std::cout << std::setw(55) << std::right << reducer_name + "(" + model_name + ") took "
-                      << relative_to(total_ns, dataset.size()) << " ns/key (" << nanoseconds_to_seconds(total_ns)
-                      << " s total)" << std::endl;
-         };
+      {
+         std::unique_lock<std::mutex> lock(iomutex);
+         std::cout << std::setw(55) << std::right << reducer_name + "(" + model_name + ") took "
+                   << relative_to(total_ns, dataset.size()) << " ns/key (" << nanoseconds_to_seconds(total_ns)
+                   << " s total)" << std::endl;
+      };
 #endif
 
-         const auto str = [](auto s) { return std::to_string(s); };
-         outfile.write({
-            {"dataset", dataset_name},
-            {"numelements", str(dataset.size())},
-            {"load_factor", str(load_factor)},
-            {"model", model_name},
-            {"reducer", reducer_name},
-            {"sample_size", str(sample_size)},
-            {"min", str(stats.min)},
-            {"max", str(stats.max)},
-            {"std_dev", str(stats.std_dev)},
-            {"empty_slots", str(stats.empty_slots)},
-            {"empty_slots_percent", str(relative_to(stats.empty_slots, hashtable_size))},
-            {"colliding_slots", str(stats.colliding_slots)},
-            {"colliding_slots_percent", str(relative_to(stats.colliding_slots, hashtable_size))},
-            {"total_colliding_keys", str(stats.total_colliding_keys)},
-            {"total_colliding_keys_percent", str(relative_to(stats.total_colliding_keys, dataset.size()))},
-            {"sample_nanoseconds_total", str(sample_ns)},
-            {"sample_nanoseconds_per_key", str(relative_to(sample_ns, dataset.size()))},
-            {"prepare_nanoseconds_total", str(prepare_ns)},
-            {"prepare_nanoseconds_per_key", str(relative_to(prepare_ns, dataset.size()))},
-            {"build_nanoseconds_total", str(build_ns)},
-            {"build_nanoseconds_per_key", str(relative_to(build_ns, dataset.size()))},
-            {"hashing_nanoseconds_total", str(stats.inference_reduction_memaccess_total_ns)},
-            {"hashing_nanoseconds_per_key",
-             str(relative_to(stats.inference_reduction_memaccess_total_ns, dataset.size()))},
-            {"total_nanoseconds", str(total_ns)},
-            {"total_nanoseconds_per_key", str(relative_to(total_ns, dataset.size()))},
-         });
-      }
+      const auto str = [](auto s) { return std::to_string(s); };
+      outfile.write({
+         {"dataset", dataset_name},
+         {"numelements", str(dataset.size())},
+         {"load_factor", str(load_factor)},
+         {"model", model_name},
+         {"reducer", reducer_name},
+         {"sample_size", str(relative_to(sample.size(), dataset.size()))},
+         {"min", str(stats.min)},
+         {"max", str(stats.max)},
+         {"std_dev", str(stats.std_dev)},
+         {"empty_slots", str(stats.empty_slots)},
+         {"empty_slots_percent", str(relative_to(stats.empty_slots, hashtable_size))},
+         {"colliding_slots", str(stats.colliding_slots)},
+         {"colliding_slots_percent", str(relative_to(stats.colliding_slots, hashtable_size))},
+         {"total_colliding_keys", str(stats.total_colliding_keys)},
+         {"total_colliding_keys_percent", str(relative_to(stats.total_colliding_keys, dataset.size()))},
+         {"sample_nanoseconds_total", str(sample_ns)},
+         {"sample_nanoseconds_per_key", str(relative_to(sample_ns, dataset.size()))},
+         {"prepare_nanoseconds_total", str(prepare_ns)},
+         {"prepare_nanoseconds_per_key", str(relative_to(prepare_ns, dataset.size()))},
+         {"build_nanoseconds_total", str(build_ns)},
+         {"build_nanoseconds_per_key", str(relative_to(build_ns, dataset.size()))},
+         {"hashing_nanoseconds_total", str(stats.inference_reduction_memaccess_total_ns)},
+         {"hashing_nanoseconds_per_key",
+          str(relative_to(stats.inference_reduction_memaccess_total_ns, dataset.size()))},
+         {"total_nanoseconds", str(total_ns)},
+         {"total_nanoseconds_per_key", str(relative_to(total_ns, dataset.size()))},
+      });
    };
 
    const auto sort_prepare = [](auto& sample) { std::sort(sample.begin(), sample.end()); };
@@ -145,37 +130,64 @@ static void measure(const std::string& dataset_name, const std::vector<uint64_t>
 
    // TODO: test different reduction methods (optimize with unlikely() annotation etc)
 
-   measure_model(
-      "pgm_eps128", sort_prepare, [](const auto& sample) { return pgm::PGMIndex<HASH_64, 128>(sample); }, //
-      [](const auto& pgm, const size_t& sample_n, const HASH_64& N, const HASH_64& key) {
-         // Since we're training pgm on a sample, pos has to be scaled to fill the full [0, N]
-         const auto sample_pos = static_cast<long double>(pgm.search(key).pos);
-         const auto fac = static_cast<long double>(N) / static_cast<long double>(sample_n);
-         return static_cast<HASH_64>(sample_pos * fac);
-      }, //
-      "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+   for (const auto sample_size : args.sample_sizes) {
+      // TODO: pgm will EXC_BAD_ACCESS (out of bounds) if we query for a key larger than the largest key from
+      //  the sample. As a fix, we ensure that min/max key is always present. Discuss a proper solution
+      //  (change algorithm? but how, i.e., extrapolate from last segment? change sampling?).
+      const auto sample_n = static_cast<size_t>(sample_size * static_cast<long double>(dataset.size()));
+      const auto pgm_sample_fn = [&]() {
+         std::vector<uint64_t> sample(sample_n, 0);
+         if (sample_n < 2)
+            return sample;
+         assert(dataset.size() > 0);
+         sample[0] = *std::min_element(dataset.begin(), dataset.end());
+         sample[1] = *std::max_element(dataset.begin(), dataset.end());
 
-   measure_model(
-      "pgm_eps16", sort_prepare, [](const auto& sample) { return pgm::PGMIndex<HASH_64, 16>(sample); }, //
-      [](const auto& pgm, const size_t& sample_n, const HASH_64& N, const HASH_64& key) {
-         // Since we're training pgm on a sample, pos has to be scaled to fill the full [0, N]
-         const auto sample_pos = static_cast<long double>(pgm.search(key).pos);
-         const auto fac = static_cast<long double>(N) / static_cast<long double>(sample_n);
-         return static_cast<HASH_64>(sample_pos * fac);
-      }, //
-      "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+         const uint64_t seed = 0x9E3779B9LU; // Random constant to ensure reproducibility
+         std::default_random_engine gen(seed);
+         std::uniform_int_distribution<uint64_t> dist(0, dataset.size() - 1);
+         for (size_t i = 0; i < sample_n - 2; i++) {
+            const auto random_index = dist(gen);
+            sample[i] = dataset[random_index];
+         }
 
-   measure_model(
-      "pgm_hash_eps128", sort_prepare,
-      [](const auto& sample) { return pgm::PGMHash<HASH_64, 128>(sample.begin(), sample.end()); }, //
-      [](const auto& pgm, const size_t& sample_n, const HASH_64& N, const HASH_64& key) { return pgm.hash(key, N); }, //
-      "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+         return sample;
+      };
 
-   measure_model(
-      "pgm_hash_eps16", sort_prepare,
-      [](const auto& sample) { return pgm::PGMHash<HASH_64, 16>(sample.begin(), sample.end()); }, //
-      [](const auto& pgm, const size_t& sample_n, const HASH_64& N, const HASH_64& key) { return pgm.hash(key, N); }, //
-      "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+      measure_model(
+         "pgm_eps128", pgm_sample_fn, sort_prepare,
+         [](const auto& sample) { return pgm::PGMIndex<HASH_64, 128>(sample); }, //
+         [&](const auto& pgm, const HASH_64& N, const HASH_64& key) {
+            // Since we're training pgm on a sample, pos has to be scaled to fill the full [0, N]
+            const auto sample_pos = static_cast<long double>(pgm.search(key).pos);
+            const auto fac = static_cast<long double>(N) / static_cast<long double>(sample_n);
+            return static_cast<HASH_64>(sample_pos * fac);
+         }, //
+         "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+
+      measure_model(
+         "pgm_eps16", pgm_sample_fn, sort_prepare,
+         [](const auto& sample) { return pgm::PGMIndex<HASH_64, 16>(sample); }, //
+         [&](const auto& pgm, const HASH_64& N, const HASH_64& key) {
+            // Since we're training pgm on a sample, pos has to be scaled to fill the full [0, N]
+            const auto sample_pos = static_cast<long double>(pgm.search(key).pos);
+            const auto fac = static_cast<long double>(N) / static_cast<long double>(sample_n);
+            return static_cast<HASH_64>(sample_pos * fac);
+         }, //
+         "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+
+      measure_model(
+         "pgm_hash_eps128", pgm_sample_fn, sort_prepare,
+         [](const auto& sample) { return pgm::PGMHash<HASH_64, 128>(sample.begin(), sample.end()); }, //
+         [](const auto& pgm, const HASH_64& N, const HASH_64& key) { return pgm.hash(key, N); }, //
+         "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+
+      measure_model(
+         "pgm_hash_eps16", pgm_sample_fn, sort_prepare,
+         [](const auto& sample) { return pgm::PGMHash<HASH_64, 16>(sample.begin(), sample.end()); }, //
+         [](const auto& pgm, const HASH_64& N, const HASH_64& key) { return pgm.hash(key, N); }, //
+         "min_max_cutoff", Reduction::min_max_cutoff<HASH_64>);
+   }
 }
 
 int main(int argc, char* argv[]) {
