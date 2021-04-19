49f7a638bdbbc091981558e351d160f8ae61c9dd
Dominik Horn <dominik.horn@tum.de>
2021-04-19 15:25:53 +0200

    implement benchmark.sh thread limit
    each collisions* thread keeps its own collisions counter vector,
    amounting > ~6GB of data for 200000000 entry datasets. To be able
    to obtain preliminary results without access to the benchmark server,
    this has to be limited.

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,7 @@ CLANGCPP_BIN=clang++
 GCC_BIN=gcc-10
 GCCPP_BIN=g++-10
 
+MAX_THREADS=2
 LOAD_FACTORS=1.0,0.75,0.5,0.25
 SAMPLE_SIZES=0.001,0.01,0.1,1.0
 DATASETS="data/books_200M_uint32:4 data/books_200M_uint64:8 data/fb_200M_uint64:8 data/osm_cellids_200M_uint64:8 data/wiki_ts_200M_uint64:8"
@@ -26,12 +27,14 @@ benchmark/throughput-${CLANGCPP_BIN} \
 benchmark/collisions_hash-${CLANGCPP_BIN} \
   --load_factors=${LOAD_FACTORS} \
   --outfile results/collisions_hash/collisions_hash-${CLANGCPP_BIN}.csv \
+  --max_threads=${MAX_THREADS} \
   $DATASETS
 
 benchmark/collisions_learned-${CLANGCPP_BIN} \
   --load_factors=${LOAD_FACTORS} \
   --sample_sizes=${SAMPLE_SIZES} \
   --outfile results/collisions_learned/collisions_learned-${CLANGCPP_BIN}.csv \
+  --max_threads=${MAX_THREADS} \
   $DATASETS
 
 # GCC benchmarks
@@ -42,11 +45,13 @@ benchmark/throughput-${GCCPP_BIN} \
 benchmark/collisions_hash-${GCCPP_BIN} \
   --load_factors=${LOAD_FACTORS} \
   --outfile results/collisions_hash/collisions_hash-${GCCPP_BIN}.csv \
+  --max_threads=${MAX_THREADS} \
   $DATASETS
 
 benchmark/collisions_learned-${GCCPP_BIN} \
   --load_factors=${LOAD_FACTORS} \
   --sample_sizes=${SAMPLE_SIZES} \
   --outfile results/collisions_learned/collisions_learned-${GCCPP_BIN}.csv \
+  --max_threads=${MAX_THREADS} \
   $DATASETS
 
