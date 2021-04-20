9d8cb6074a40208c1bf3d1fef1102b38967a0f6c
Dominik Horn <dominik.horn@tum.de>
2021-04-20 13:22:18 +0200

    generate nanoseconds_per_key stats for collisions_hash

--- results/collisions_hash/plots.py
+++ results/collisions_hash/plots.py
@@ -25,12 +25,16 @@ def gen_colors(max=40.0, s_sawtooth_min=0.7, s_sawtooth_max=0.9, s_sawtooth_step
             v_next = v_sawtooth_min
 
 
-compilers = ["clang++", "g++-10"]
+compilers = ["clang++"] #, "g++-10"]
 _csv = pandas.read_csv(f"collisions_hash-{compilers[0]}.csv")
 dataset_names = sorted(set(_csv['dataset']))
 hash_methods = list(OrderedDict.fromkeys(list(_csv[_csv['reducer'] != 'do_nothing'].sort_values('nanoseconds_per_key')['hash'])))
 _colors = list(gen_colors(len(hash_methods)))
 colors = {method: _colors[i] for i, method in enumerate(hash_methods)}
+
+# ============================
+#    colliding keys percent
+# ============================
 
 for compiler in compilers:
     csv = pandas.read_csv(f'collisions_hash-{compiler}.csv')
@@ -71,3 +75,44 @@ for compiler in compilers:
 
         plt.savefig(f"graphs/colliding_keys_percent_{dataset_name}_{compiler}.png", bbox_inches='tight', pad_inches=0.5)
         plt.savefig(f"graphs/colliding_keys_percent_{dataset_name}_{compiler}.pdf", bbox_inches='tight', pad_inches=0.5)
+
+# ============================
+#    nanoseconds per key
+# ============================
+
+for compiler in compilers:
+    csv = pandas.read_csv(f'collisions_hash-{compiler}.csv')
+
+    for fig, dataset_name in enumerate(dataset_names):
+        dataset = csv[csv['dataset'] == dataset_name]
+
+        plt.figure(figsize=(40, 12))
+        plt.subplots_adjust(hspace=0.5)
+
+        for k, load_factor in enumerate(reversed(sorted(set(dataset['load_factor'])))):
+            ds = dataset[dataset['load_factor'] == load_factor]
+            ds = ds[ds['reducer'] != 'do_nothing']
+            reducers = sorted(list(set(ds['reducer'])))
+
+            plt.subplot(2, 2, k + 1)
+
+            # order preserving deduplication
+            for j, hash_name in enumerate([m for m in hash_methods if m in set(ds['hash'])]):
+                series = list(ds[ds['hash'] == hash_name].sort_values('reducer')['nanoseconds_per_key'])
+                width = 0.95 / len(hash_methods)
+
+                for i, reducer in enumerate(reducers):
+                    # We only want to set label once as otherwise legend will contain duplicates
+                    plt.bar(i + j * width + j * 0.005, series[i], width, label=hash_name if i == 0 else None,
+                            color=colors.get(hash_name) or "white")
+
+            plt.grid(linestyle='--', linewidth=0.5)
+            plt.title(f"nanoseconds per key on {dataset_name}, load_factor {load_factor}, compiler {compiler}")
+            plt.xticks(np.arange(len(reducers)) + (1.0 / len(reducers)), reducers)
+            plt.ylabel('nanoseconds per key')
+            plt.xlabel('hash reduction method')
+
+            plt.legend(bbox_to_anchor=(0.5, -0.1), loc="upper center", ncol=7)
+
+        plt.savefig(f"graphs/nanoseconds_per_key_{dataset_name}_{compiler}.png", bbox_inches='tight', pad_inches=0.5)
+        plt.savefig(f"graphs/nanoseconds_per_key_{dataset_name}_{compiler}.pdf", bbox_inches='tight', pad_inches=0.5)
