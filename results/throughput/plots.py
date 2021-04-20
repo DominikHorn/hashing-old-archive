0a78a538e65bff4bdcc7796157b8c37687e7f419
Dominik Horn <dominik.horn@tum.de>
2021-04-20 12:36:29 +0200

    generate plots based on results from new measurements
    since these superseed old results, they are deleted with this commit

--- results/throughput/plots.py
+++ results/throughput/plots.py
@@ -1,45 +1,62 @@
+import colorsys
 from collections import OrderedDict
 
-import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pandas
 
-csv = pandas.read_csv('throughput.csv')
-dataset_names = sorted(set(csv['dataset']))
 
-# ensure colors are at least consistent across plots
-tableau_colors = list(matplotlib.colors.TABLEAU_COLORS)
-hash_methods = list(
-    OrderedDict.fromkeys(list(
-        csv[csv['load_factor'] == 1.0][csv['reducer'] == 'do_nothing'].sort_values('nanoseconds_per_key')['hash'])))
-colors = {method: tableau_colors[i % len(tableau_colors)] for i, method in enumerate(hash_methods)}
+def gen_colors(max=40.0, s_sawtooth_min=0.7, s_sawtooth_max=0.9, s_sawtooth_step=0.1, v_sawtooth_min=0.5,
+               v_sawtooth_max=1.0, v_sawtooth_step=0.15):
+    s_next = s_sawtooth_min
+    v_next = v_sawtooth_min
+    for i in range(max):
+        h = (2 * i / max) if 2 * i <= max else (2 * i - max) / max
+        s = s_next
+        v = v_next
+        yield colorsys.hsv_to_rgb(h, s, v)
 
-for fig, dataset_name in enumerate(dataset_names):
-    dataset = csv[csv['dataset'] == dataset_name]
-    dataset = dataset[dataset['load_factor'] == 1.0]
+        s_next += s_sawtooth_step
+        if s_next > s_sawtooth_max:
+            s_next = s_sawtooth_min
 
-    reducers = sorted(list(set(dataset['reducer'])))
+        v_next += v_sawtooth_step
+        if v_next > v_sawtooth_max:
+            v_next = v_sawtooth_min
 
-    plt.figure(figsize=(30, 10))
 
-    # order preserving deduplication
-    for j, hash_name in enumerate([m for m in hash_methods if m in set(dataset['hash'])]):
-        series = list(dataset[dataset['hash'] == hash_name].sort_values('reducer')['nanoseconds_per_key'])
-        width = 0.95 / len(hash_methods)
+for compiler in ["clang++", "g++-10"]:
+    csv = pandas.read_csv(f"throughput-{compiler}.csv")
+    dataset_names = sorted(set(csv['dataset']))
+    hash_methods = list(OrderedDict.fromkeys(list(csv[csv['reducer'] != 'do_nothing'].sort_values('nanoseconds_per_key')['hash'])))
 
-        for i, reducer in enumerate(reducers):
-            # We only want to set label once as otherwise legend will contain duplicates
-            plt.bar(i + j * width, series[i], width, label=hash_name if i == 0 else None,
-                    color=colors.get(hash_name) or "white")
+    _colors = list(gen_colors(len(hash_methods)))
+    colors = {method: _colors[i] for i, method in enumerate(hash_methods)}
 
-    plt.grid(linestyle='--', linewidth=0.5)
-    plt.title(f"throughput on {dataset_name}")
-    plt.xticks(np.arange(len(reducers)) + 0.4, reducers)
-    plt.yticks(list(plt.yticks()[0]) + [1, 2, 3, 4])
-    plt.ylabel('ns per key')
-    plt.xlabel('hash reduction method')
-    plt.legend(bbox_to_anchor=(0.5, -0.1), ncol=7)
+    for fig, dataset_name in enumerate(dataset_names):
+        dataset = csv[csv['dataset'] == dataset_name]
 
-    plt.savefig(f"throughput_{dataset_name}.pdf", bbox_inches='tight', pad_inches=0.5)
-    plt.savefig(f"throughput_{dataset_name}.png", bbox_inches='tight', pad_inches=0.5)
+        reducers = sorted(list(set(dataset['reducer'])))
+
+        plt.figure(figsize=(30, 10))
+
+        # order preserving deduplication
+        for j, hash_name in enumerate([m for m in hash_methods if m in set(dataset['hash'])]):
+            series = list(dataset[dataset['hash'] == hash_name].sort_values('reducer')['nanoseconds_per_key'])
+            width = 0.95 / len(hash_methods)
+
+            for i, reducer in enumerate(reducers):
+                # We only want to set label once as otherwise legend will contain duplicates
+                plt.bar(i + j * width, series[i], width, label=hash_name if i == 0 else None,
+                        color=colors.get(hash_name) or "white")
+
+        plt.grid(linestyle='--', linewidth=0.5)
+        plt.title(f"throughput on {dataset_name}")
+        plt.xticks(np.arange(len(reducers)) + 0.4, reducers)
+        plt.yticks(list(plt.yticks()[0]) + [1, 2, 3, 4])
+        plt.ylabel('ns per key')
+        plt.xlabel('hash reduction method')
+        plt.legend(bbox_to_anchor=(0.5, -0.1), ncol=7)
+
+        plt.savefig(f"graphs/throughput-{compiler}_{dataset_name}.pdf", bbox_inches='tight', pad_inches=0.5)
+        plt.savefig(f"graphs/throughput-{compiler}_{dataset_name}.png", bbox_inches='tight', pad_inches=0.5)
