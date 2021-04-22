13e0c5518495c2d631ca2084841bddd94f24dddc
Dominik Horn <dominik.horn@tum.de>
2021-04-22 22:25:15 +0200

    add hashtable_learned benchmark to {build,benchmark}.sh
    also deduplicate/rework these scripts

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # SET THESE ACCORDING TO YOUR SYSTEM CONFIG
-CLANG_BIN=clang
-CLANGCPP_BIN=clang++
-GCC_BIN=gcc-10
-GCCPP_BIN=g++-10
-
 MAX_THREADS=2
 LOAD_FACTORS=1.0,0.75,0.5,0.25
 SAMPLE_SIZES=0.001,0.01,0.1,1.0
 DATASETS="data/books_200M_uint32:4 data/books_200M_uint64:8 data/fb_200M_uint64:8 data/osm_cellids_200M_uint64:8 data/wiki_ts_200M_uint64:8"
-
 
 # Stop on error & cd to script directory
 set -e
@@ -19,67 +13,49 @@ cd "$(dirname "$0")"
 # Ensure binaries are freshly built
 ./build.sh
 
+# Ensure output directory exists
 mkdir -p results/{throughput_hash,throughput_learned,collisions_hash,collisions_learned,hashtable_hash,hashtable_learned}
 
-# Clang benchmarks
-benchmark/throughput_hash-${CLANGCPP_BIN} \
-  --outfile results/throughput_hash/throughput_hash-${CLANGCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+# Build with various compilers. SET THIS ACCORDING TO YOUR SYSTEM CONFIG
+compilers=(clang++ g++-10)
+for c in clang,clang++ gcc-10,g++-10
+do
+  IFS=","
+  set -- $c
 
-benchmark/throughput_learned-${CLANGCPP_BIN} \
-  --outfile results/throughput_learned/throughput_learned-${CLANGCPP_BIN}.csv \
-  --sample_sizes=${SAMPLE_SIZES} \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+  benchmark/throughput_hash-${2} \
+    --outfile results/throughput_hash/throughput_hash-${2}.csv \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
 
-benchmark/collisions_hash-${CLANGCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --outfile results/collisions_hash/collisions_hash-${CLANGCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+  benchmark/throughput_learned-${2} \
+    --outfile results/throughput_learned/throughput_learned-${2}.csv \
+    --sample_sizes=${SAMPLE_SIZES} \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
 
-benchmark/collisions_learned-${CLANGCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --sample_sizes=${SAMPLE_SIZES} \
-  --outfile results/collisions_learned/collisions_learned-${CLANGCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+  benchmark/collisions_hash-${2} \
+    --load_factors=${LOAD_FACTORS} \
+    --outfile results/collisions_hash/collisions_hash-${2}.csv \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
 
-benchmark/hashtable_hash-${CLANGCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --outfile results/hashtable_hash/hashtable_hash-${CLANGCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+  benchmark/collisions_learned-${2} \
+    --load_factors=${LOAD_FACTORS} \
+    --sample_sizes=${SAMPLE_SIZES} \
+    --outfile results/collisions_learned/collisions_learned-${2}.csv \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
 
-# GCC benchmarks
-benchmark/throughput_hash-${GCCPP_BIN} \
-  --outfile results/throughput_hash/throughput_hash-${GCCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
+  benchmark/hashtable_hash-${2} \
+    --load_factors=${LOAD_FACTORS} \
+    --outfile results/hashtable_hash/hashtable_hash-${2}.csv \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
 
-benchmark/throughput_learned-${GCCPP_BIN} \
-  --outfile results/throughput_learned/throughput_learned-${GCCPP_BIN}.csv \
-  --sample_sizes=${SAMPLE_SIZES} \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
-
-benchmark/collisions_hash-${GCCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --outfile results/collisions_hash/collisions_hash-${GCCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
-
-benchmark/collisions_learned-${GCCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --sample_sizes=${SAMPLE_SIZES} \
-  --outfile results/collisions_learned/collisions_learned-${GCCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
-
-benchmark/hashtable_hash-${GCCPP_BIN} \
-  --load_factors=${LOAD_FACTORS} \
-  --outfile results/hashtable_hash/hashtable_hash-${GCCPP_BIN}.csv \
-  --max_threads=${MAX_THREADS} \
-  $DATASETS
-
+  benchmark/hashtable_learned-${2} \
+    --load_factors=${LOAD_FACTORS} \
+    --outfile results/hashtable_learned/hashtable_learned-${2}.csv \
+    --max_threads=${MAX_THREADS} \
+    $DATASETS
+done
