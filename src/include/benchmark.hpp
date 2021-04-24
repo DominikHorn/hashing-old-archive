1d16a26fcb632461f7d461774d3685969be4b406
Dominik Horn <dominik.horn@tum.de>
2021-04-24 12:37:25 +0200

    emulate performing work with ht payload on lookup
    ... using full memory barrier

--- src/include/benchmark.hpp
+++ src/include/benchmark.hpp
@@ -136,7 +136,8 @@ namespace Benchmark {
       for (const auto& key : dataset) {
          auto payload = ht.lookup(key, hashfn);
          Optimizer::DoNotEliminate(payload);
-         assert(payload == key - 5);
+         assert(payload == key - 5); // debug only
+         full_mem_barrier // emulate doing something with payload, i.e., wait for it!
       }
       end_time = std::chrono::steady_clock::now();
       uint64_t total_lookup_ns =
