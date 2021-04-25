5b9a175c689d9da9ebd75701ef8bb1ea1c8ebc56
Dominik Horn <dominik.horn@tum.de>
2021-04-25 19:04:36 +0200

    add more pgm variations results to collisions experiment

--- results/collisions_learned/plots.py
+++ results/collisions_learned/plots.py
@@ -31,7 +31,7 @@ compilers = ["g++"]  # ["clang++", "g++"]
 _csv = pandas.read_csv(f"collisions_learned-{compilers[0]}.csv")
 dataset_names = sorted(set(_csv['dataset']))
 models = list(
-    OrderedDict.fromkeys(list(_csv[_csv['reducer'] != 'do_nothing'].sort_values('total_nanoseconds_per_key')['model'])))
+    OrderedDict.fromkeys(list(_csv[_csv['reducer'] != 'do_nothing'].sort_values('model')['model'])))
 _colors = list(gen_colors(len(models)))
 colors = {method: _colors[i] for i, method in enumerate(models)}
 
