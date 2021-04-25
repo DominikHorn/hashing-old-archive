390c90b2a141d1b46b40cc225f3fd6040381e5ff
Dominik Horn <dominik.horn@tum.de>
2021-04-25 17:04:50 +0200

    visualize hashtable_hash results

---
+++ results/hashtable_hash/README.md
@@ -0,0 +1,87 @@
+# Hashtable Learned
+
+Results for the hashtable experiment using classical hash functions. Currently, the only tested hashtable implementation
+is a chained hashtable with varying bucket sizes. The experiment (in pseudocode) measures insert and lookup performance
+as follows:
+
+```
+// Insert in tight loop
+for key in dataset {
+    index = reduce(hash(key))
+    hashtable.insert(index, payload)
+}
+
+// Lookup. Simulate working with payload through full memory fence
+for key in dataset {
+    index = reduce(hash(key))
+    payload = hashtable.lookup(index, key)
+    __sync_synchronize() // gcc full memory fence builtin
+}
+```
+
+## Insert
+
+Time to insert each key in a tight loop:
+
+```
+for key in dataset {
+    index = reduce(hash(key))
+    hashtable.insert(index, payload)
+}
+```
+
+### g++ (Ubuntu 9.3.0-23ubuntu1~16.04) 9.3.0 Intel(R) Core(TM) i9-9900K 16 Core @ 3.60GHz
+
+#### books_200M_uint32
+
+![books_200M_uint32](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/insert_books_200M_uint32_g++.png)
+
+#### books_200M_uint64
+
+![books_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/insert_books_200M_uint64_g++.png)
+
+#### fb_200M_uint64
+
+![fb_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/insert_fb_200M_uint64_g++.png)
+
+#### osm_cellids_200M_uint64
+
+![osm_cellids_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/insert_osm_cellids_200M_uint64_g++.png)
+
+#### wiki_ts_200M_uint64
+
+![wiki_ts_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/insert_wiki_ts_200M_uint64_g++.png)
+
+## Lookup
+
+Time to lookup each payload, simulating working with the payload by using a memory fence:
+
+```
+for key in dataset {
+    index = reduce(hash(key))
+    payload = hashtable.lookup(index, key)
+    __sync_synchronize() // gcc full memory fence builtin
+}
+```
+
+### g++ (Ubuntu 9.3.0-23ubuntu1~16.04) 9.3.0 Intel(R) Core(TM) i9-9900K 16 Core @ 3.60GHz
+
+#### books_200M_uint32
+
+![books_200M_uint32](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/lookup_books_200M_uint32_g++.png)
+
+#### books_200M_uint64
+
+![books_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/lookup_books_200M_uint64_g++.png)
+
+#### fb_200M_uint64
+
+![fb_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/lookup_fb_200M_uint64_g++.png)
+
+#### osm_cellids_200M_uint64
+
+![osm_cellids_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/lookup_osm_cellids_200M_uint64_g++.png)
+
+#### wiki_ts_200M_uint64
+
+![wiki_ts_200M_uint64](https://github.com/andreaskipf/hashing/blob/main/results/hashtable_hash/graphs/lookup_wiki_ts_200M_uint64_g++.png)
---
+++ results/hashtable_hash/plots.py
@@ -0,0 +1,90 @@
+import colorsys
+from collections import OrderedDict
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pandas
+
+
+def gen_colors(max=40.0, s_sawtooth_min=0.7, s_sawtooth_max=0.9, s_sawtooth_step=0.1, v_sawtooth_min=0.5,
+               v_sawtooth_max=1.0, v_sawtooth_step=0.15):
+    s_next = s_sawtooth_min
+    v_next = v_sawtooth_min
+    for i in range(max):
+        h = i / max
+        s = s_next
+        v = v_next
+        yield colorsys.hsv_to_rgb(h, s, v)
+
+        s_next += s_sawtooth_step
+        if s_next > s_sawtooth_max:
+            s_next = s_sawtooth_min
+
+        v_next += v_sawtooth_step
+        if v_next > v_sawtooth_max:
+            v_next = v_sawtooth_min
+
+
+compilers = ["g++"]  # ["clang++", "g++"]
+_csv = pandas.read_csv(f"hashtable_hash-{compilers[0]}.csv")
+dataset_names = sorted(set(_csv['dataset']))
+hash_functions = list(
+    OrderedDict.fromkeys(list(_csv.sort_values('lookup_nanoseconds_total')['hash'])))
+_colors = list(gen_colors(len(hash_functions)))
+colors = {method: _colors[i] for i, method in enumerate(hash_functions)}
+
+
+def gen_plot(key):
+    for compiler in compilers:
+        csv = pandas.read_csv(f'hashtable_hash-{compiler}.csv')
+
+        for fig, dataset_name in enumerate(dataset_names):
+            dataset = csv[csv['dataset'] == dataset_name]
+            load_factors = sorted(set(dataset['load_factor']))
+            bucket_sizes = sorted(set(dataset['bucket_size']))
+
+            fig, subplts = plt.subplots(len(bucket_sizes), len(load_factors), sharex=True, sharey=True,
+                                        figsize=(20, 10))
+
+            for l, load_factor in enumerate(reversed(load_factors)):
+                for k, bucket_size in enumerate(reversed(bucket_sizes)):
+                    ds = dataset[dataset['bucket_size'] == bucket_size]
+                    ds = ds[ds['load_factor'] == load_factor]
+                    reducers = sorted(list(set(ds['reducer'])))
+
+                    subplt = subplts[k, l]
+
+                    # order preserving deduplication
+                    for j, hash_name in enumerate([m for m in hash_functions if m in set(ds['hash'])]):
+                        series = list(
+                            ds[ds['hash'] == hash_name].sort_values('reducer')[f'{key}_nanoseconds_per_key'])
+                        width = 0.95 / len(hash_functions)
+
+                        for i, reducer in enumerate(reducers):
+                            # We only want to set label once as otherwise legend will contain duplicates
+                            subplt.bar(i + j * width + j * 0.005, series[i], width,
+                                       label=hash_name if i == 0 and l == 0 and k == 0 else None,
+                                       color=colors.get(hash_name) or "white")
+
+                    subplt.grid(axis='y', linestyle='--', linewidth=0.5)
+                    subplt.set_title(f"bucket_size {bucket_size}, load_factor {load_factor} (1% sample)")
+
+                    xticks = np.arange(len(reducers)) + 0.25
+                    subplt.set_xticks(xticks, minor=False)
+                    subplt.set_xticklabels(reducers, fontdict=None, minor=False)
+                    subplt.axhline(1 - pow(math.e, -load_factor), color="blue", ls="-")
+
+                fig.text(0.5, 0.04, 'reduction algorithm', ha='center', va='center')
+                fig.text(0.06, 0.5, 'nanoseconds per key', ha='center', va='center', rotation='vertical')
+                fig.suptitle(
+                    f"hashtable {key} on {dataset_name} using compiler {compiler}")
+                fig.legend(bbox_to_anchor=(0.3, 0), loc="lower left", ncol=3)
+
+            plt.savefig(f"graphs/{key}_{dataset_name}_{compiler}.png", bbox_inches='tight', pad_inches=0.5)
+            plt.savefig(f"graphs/{key}_{dataset_name}_{compiler}.pdf", bbox_inches='tight', pad_inches=0.5)
+            plt.close()
+
+
+gen_plot('lookup')
+gen_plot('insert')
