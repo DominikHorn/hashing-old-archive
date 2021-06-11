bf5e2258576d43385f22975895998245dbff3fdf
Dominik Horn <dominik.horn@tum.de>
2021-06-11 21:24:11 +0200

    break y axis on probe time overall plot

--- short_paper_graphs/probe_times_overall/plot.py
+++ short_paper_graphs/probe_times_overall/plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import pandas as pd
 import math as math
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Latex figure export
 mpl.use("pgf")
@@ -66,9 +67,6 @@ def plot(payload_size):
     learned_hashfns = list(dict.fromkeys(cuckoo_data[cuckoo_data[REDUCER_KEY].str.match(CLAMP)][HASH_KEY])) 
     all_hashfns = learned_hashfns + classical_hashfns
 
-    # Generate plot
-    fig, ax = plt.subplots(figsize=(7.00697/2,2.3))
-
     # Aggregate data over multiple datasets
     datasets =  [
             # synthetic
@@ -81,6 +79,41 @@ def plot(payload_size):
             'wiki_200M_uint64',
             'fb_200M_uint64', 
             'osm_200M_uint64']
+
+    # Generate plot
+    fig, ax = plt.subplots(figsize=(7.00697/2,2.3))
+
+    # break y-axis
+    divider = make_axes_locatable(ax)
+    ax2 = divider.new_vertical(size="100%", pad=0.1)
+    fig.add_axes(ax2)
+
+    # tune this for each plot
+    xticks=[i+0.5 for i in range(0, len(datasets))]
+    ylim1 = [0,250] if payload_size == 16 else [0,350]
+    ax.set_ylim(ylim1[0],ylim1[1])
+    ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels([d.replace(r"_200M",
+        "").replace("_uint64", "").replace("_", " ") for d in datasets],
+        va="center_baseline",position=(0.5,-0.05), fontsize=15, rotation=35, ha="right")
+    ax.spines['top'].set_visible(False)
+    ylim2 = [400,800] if payload_size == 16 else [550,1000]
+    ax2.set_ylim(ylim2[0],ylim2[1])
+    ax2.tick_params(bottom=False, labelbottom=False)
+    ax2.tick_params(axis='both', which='major', labelsize=8)
+    ax2.spines['bottom'].set_visible(False)
+
+    # From https://matplotlib.org/examples/pylab_examples/broken_axis.html
+    d = .015  # how big to make the diagonal lines in axes coordinates
+    # arguments to pass to plot, just so we don't keep repeating them
+    kwargs = dict(transform=ax2.transAxes, color='k', clip_on=False)
+    ax2.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+    ax2.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+
+    kwargs.update(transform=ax.transAxes)  # switch to the bottom axes
+    ax.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+    ax.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
 
     for i, dataset in enumerate(datasets):
         cuckoo = cuckoo_data[cuckoo_data[DATASET_KEY] == dataset]
@@ -97,29 +130,30 @@ def plot(payload_size):
         bar_width = (0.8 - empty_space) / (len(cuckoo_bars.items()) + len(chained_bars.items()))
         gap_width = 0.05 #0.1 / len(plt_data)
         for j, (hname, probe_time) in enumerate(cuckoo_bars.items()):
-            ax.bar(i + j * (bar_width+gap_width), probe_time, bar_width, color=colors.get(hname) or "purple", edgecolor=colors.get(hname) or "purple")
+            if len(probe_time) == 0:
+                print("Missing datapoint:", hname, probe_time, "cuckoo", dataset)
+                continue
+            if len(probe_time) > 1:
+                print("ERR, TO MANY DATAPOINTS FOR SINGLE BAR:", hname, probe_time, "cuckoo", dataset)
+                exit()
+            for a in {ax, ax2}:
+                a.bar(i + j * (bar_width+gap_width), probe_time, bar_width, color=colors.get(hname) or "purple", edgecolor=colors.get(hname) or "purple")
 
         for k, (hname, probe_time) in enumerate(chained_bars.items()):
+            if len(probe_time) == 0:
+                print("Missing datapoint:", hname, probe_time, "chained", dataset)
+                continue
+            if len(probe_time) > 1:
+                print("ERR, TO MANY DATAPOINTS FOR SINGLE BAR:", hname, probe_time, "chained", dataset)
+                exit()
             j = k + len(cuckoo_bars.items())
-            ax.bar(i + j * (bar_width+gap_width), probe_time, bar_width, edgecolor=colors.get(hname) or "purple", fill=False)
+            for a in {ax, ax2}:
+                a.bar(i + j * (bar_width+gap_width), probe_time, bar_width, edgecolor=colors.get(hname) or "purple", fill=False)
 
 
-    ## Plot style/info
-    ymax=800 if payload_size == 16 else 1000
-    yticks = np.linspace(0, ymax, 5)
-    plt.ylim(0,ymax)
-    plt.ylabel(f"Probe time [ns]", fontsize=15)
-
-    plt.xticks([i+0.5 for i in range(0, len(datasets))], [d.replace(r"_200M",
-        "").replace("_uint64", "").replace("_", " ") for d in datasets],
-        va="center_baseline",position=(0.5,-0.05), fontsize=15, rotation=35, ha="right")
-    plt.xlabel("Dataset", fontsize=15)
-
-    #plt.margins(x=0.01,y=0.2)
     plt.tight_layout()
-    plt.subplots_adjust(bottom=0.4, top=0.95)
 
-    ax.legend(
+    ax2.legend(
         handles=[mpatches.Patch(color=colors.get(h), label=name(h)) for h in all_hashfns] +
             [mpatches.Rectangle((0,0), 10, 5, color='black', label="Cuckoo"),
                mpatches.Rectangle((0,0), 10, 5, color='black', label="Chained", fill=False) ],
@@ -127,6 +161,10 @@ def plot(payload_size):
         fontsize=6,
         labelspacing=0.1,
         ncol=2)
+
+
+    fig.text(0.5, 0.02, 'Dataset', ha='center', fontsize=8)
+    fig.text(0.01, 0.5, 'Probe time per key [ns]', va='center', rotation='vertical', fontsize=8)
 
     plt.savefig(f"out/probe_times_overall_{payload_size}.pdf")
 
